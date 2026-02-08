5d827267e388912ca61c42bf812489395fba1937
Ciro Santilli <cirosantilli@proton.me>
2026-02-08 19:38:21 +0000

    Fix p54

--- solvers/54.lean
+++ solvers/54.lean
@@ -4,4 +4,5 @@ open ProjectEulerSolutions.P54
 
 def main : IO Unit := do
   let text ← IO.FS.readFile "0054_poker.txt"
-  IO.println (solveLines (parseLines text))
+  let lines := parseLines text
+  IO.println (solveLines lines)
