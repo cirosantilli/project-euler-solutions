a341b900ba27579d8352666efa3ac42e8e36bcb4
Ciro Santilli <cirosantilli@proton.me>
2026-03-01 18:17:05 +0000

    eulersolve.org

---
+++ scrape-eulersolve-org.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eux
+d=tmp/eulersolve
+mkdir -p "$d"
+for i in `seq 1 986`; do
+  echo $i
+  #wget -O "$d/$i.cpp" https://eulersolve.org/solutionsCpp/Euler$i.cpp
+  sed -i "1s|^|// https://eulersolve.org/solutionsCpp/Euler$i.cpp\n\n|" "$d/$i.cpp"
+done
