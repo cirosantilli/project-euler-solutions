f0f42c752b417b498f7d1acd9f353e56eaf5eb66
Ciro Santilli <cirosantilli@proton.me>
2026-03-03 06:46:23 +0000

    scrape wget restore

--- scrape-eulersolve-org.sh
+++ scrape-eulersolve-org.sh
@@ -4,6 +4,6 @@ d=tmp/eulersolve
 mkdir -p "$d"
 for i in `seq 1 986`; do
   echo $i
-  #wget -O "$d/$i.cpp" https://eulersolve.org/solutionsCpp/Euler$i.cpp
+  wget -O "$d/$i.cpp" https://eulersolve.org/solutionsCpp/Euler$i.cpp
   sed -i "1s|^|// https://eulersolve.org/solutionsCpp/Euler$i.cpp\n\n|" "$d/$i.cpp"
 done
