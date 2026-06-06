33ec5b69d7cc0949438da6c442ffb8e3bdf42321
Ciro Santilli <cirosantilli@proton.me>
2026-06-06 10:13:34 +0100

    Remove partial from P2.lean

--- ProjectEulerSolutions/P2.lean
+++ ProjectEulerSolutions/P2.lean
@@ -2,14 +2,17 @@ import ProjectEulerStatements.P2
 
 namespace ProjectEulerSolutions.P2
 
-partial def solve (limit : Nat) : Nat :=
-  let rec go (a b total : Nat) : Nat :=
+def solve (limit : Nat) : Nat :=
+  let rec go (a b total : Nat) (ha : 0 < a) (hab : a < b) : Nat :=
     if a ≤ limit then
       let total' := if a % 2 = 0 then total + a else total
-      go b (a + b) total'
+      go b (a + b) total' (by omega) (by omega)
     else
       total
-  go 1 2 0
+  termination_by limit + 1 - a
+  decreasing_by
+    omega
+  go 1 2 0 (by omega) (by omega)
 
 example : solve 4000000 = 4613732 := by native_decide
 end ProjectEulerSolutions.P2
