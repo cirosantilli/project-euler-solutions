ced368cfc21f01df97441455cb6f4148ef775928
Ciro Santilli <cirosantilli@proton.me>
2026-06-06 08:59:47 +0100

    Correctly sorry P19

--- ProjectEulerSolutions/Equiv/P19.lean
+++ ProjectEulerSolutions/Equiv/P19.lean
@@ -2,5 +2,5 @@ import ProjectEulerSolutions.P19
 namespace ProjectEulerSolutions.P19
 
 theorem equiv (startDate endDate : ProjectEulerStatements.P19.Date) :
-    ProjectEulerStatements.P19.naive startDate endDate = solve startDate endDate := rfl
+    ProjectEulerStatements.P19.naive startDate endDate = solve startDate endDate := sorry
 end ProjectEulerSolutions.P19
